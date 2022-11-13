64af5c1972e0a535afd18221a306fd1693820df8
bushi88 <108837848+bushi88@users.noreply.github.com>
2022-11-13 17:13:53 +0100

    Add files via upload

---
+++ js/fusee.js
@@ -0,0 +1,12 @@
+/*function depart(){
+    let TA = document.getElementById('textAni') ;
+    TA.style.color = "green" ;
+    let img = document.getElementById('nighsky');
+    img.style.animationDirection = "alternate" ;
+    
+}*/
+
+$("button").on('click',function(){
+    $("#fusee").animate({left: '250px'});
+    $("#textAni").animate({down: '400px'});
+    });  
